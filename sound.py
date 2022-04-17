6873d3646211ca1a3b884db64da07d76fd8a8928
dj <djesc@139.com>
2022-04-17 15:05:12 +0800

    update

--- sound.py
+++ sound.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 from playsound import playsound
+import winsound
+# from pydub.playback import play
 
 
 class Sound():
 
     def __init__(self, filename) -> None:
         self.filename = filename
+        # self.song=AudioSegment.from_wav(self.filename)
         pass
 
     def play(self):
-        playsound(self.filename, False)
+        winsound.PlaySound(self.filename, winsound.SND_FILENAME)
         return
